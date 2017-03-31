6d61fe4ff9a3fd9132b38007dea5a5a138415536
Suryan5h <suryanshbhardwaj46@gmail.com>
2017-04-01 01:21:37 +0530

    Create MainActivity.java

---
+++ MainActivity.java
@@ -0,0 +1,45 @@
+package com.example.suryanshbhardwaj.qrcodescannerexample;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import com.google.zxing.Result;
+import me.dm7.barcodescanner.zxing.ZXingScannerView;
+
+public class MainActivity extends Activity implements ZXingScannerView.ResultHandler {
+
+    private ZXingScannerView mScannerView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void onClick(View v){
+        mScannerView = new ZXingScannerView(this);
+        setContentView(mScannerView);
+        mScannerView.setResultHandler(this);
+        mScannerView.startCamera();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mScannerView.stopCamera();
+    }
+
+    @Override
+    public void handleResult(Result result) {
+        //Do anything with result here :D
+        Log.w("handleResult",result.getText( ));
+        AlertDialog.Builder builder= new AlertDialog.Builder(this);
+        builder.setTitle("Scan Result");
+        builder.setMessage(result.getText());
+        AlertDialog alertDialog = builder.create();
+        alertDialog.show();
+
+        //Resume scanning uncomment below
+        //mScannerView.resumeCameraPreview(this);
+    }
+}
